51ffe23188b48910de4921dd93b75c5e0da9eb3d
Gabi Volpe <gabriel.volpe@paddypower.com>
2015-09-09 16:27:30 +0100

    Added Future for-comprehensions App.

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ Simple comparison of the use of the standard Scala Future and the [scala async l
 
 ## Benchmarks
 
-After running every application (both FutureApp and AsyncAwaitApp) 10 times, this are the results for the average time performing the sum of three simple Math.sqrt operations:
+After running every application 10 times, this are the results for the average time performing the sum of three simple Math.sqrt operations:
 
-#### Future
+#### Future Sequence
 * 17.3 ms (16 15 20 15 13 14 12 26 22 20)
+
+#### Future for-comprehension
+* 13.4 ms (17 9 8 11 14 17 8 19 12 19)
 
 #### AsyncWait
 * 6 ms (6 9 5 5 6 8 5 6 5 5)
+
+The machine used to run this benchark is an Intel® Xeon(R) CPU X5687 @ 3.60GHz × 4 with 23.5 GiB of memory running on Ubuntu 14.04 LTS 64 bits.
--- src/main/scala/com/gvolpe/computations/FutureSequenceApp.scala
+++ src/main/scala/com/gvolpe/computations/FutureSequenceApp.scala
@@ -1,24 +1,19 @@
 package com.gvolpe.computations
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-object FutureApp extends App {
+object FutureSequenceApp extends App {
 
   val (n1, n2, n3) = (Future(Math.sqrt(7891.12)), Future(Math.sqrt(995522.54)), Future(Math.sqrt(359.87)))
 
   val start = System.currentTimeMillis()
 
-  val f = for {
-    r1 <- n1
-    r2 <- n2
-    r3 <- n3
-  } yield (r1 + r2 + r3)
-
+  val f = Future.sequence(Seq(n1, n2, n3))
   f.onComplete {
     case Success(result) =>
-      println(s"SUM ${result}")
+      println(s"SUM ${result.sum}")
       println(s"TIME (ms): ${System.currentTimeMillis() - start}")
     case Failure(e) => println(s"ERROR: ${e.getMessage}")
   }
