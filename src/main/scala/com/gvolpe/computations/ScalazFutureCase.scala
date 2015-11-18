7093a47b684b21ba0c68375d6dd814da7d6391f8
Jeff Shaw <shawjef3@gmail.com>
2015-11-17 21:46:35 -0500

    construct the tasks outside of the benchmarks

--- src/main/scala/com/gvolpe/computations/ScalazFutureCase.scala
+++ src/main/scala/com/gvolpe/computations/ScalazFutureCase.scala
@@ -5,8 +5,10 @@ import scalaz.Scalaz._
 import scalaz.concurrent.Future
 
 object ScalazFutureCase extends SumResult {
+
+  val futures = Vector(Future(Math.sqrt(7891.12)), Future(Math.sqrt(995522.54)), Future(Math.sqrt(359.87)))
+
   override def sumResult: Double = {
-    val futures = Vector(Future(Math.sqrt(7891.12)), Future(Math.sqrt(995522.54)), Future(Math.sqrt(359.87)))
     val sum = for {
       v <- futures.sequence[Future, Double]
     } yield v.sum
--- src/main/scala/com/gvolpe/computations/ScalazTaskCase.scala
+++ src/main/scala/com/gvolpe/computations/ScalazTaskCase.scala
@@ -5,8 +5,10 @@ import scalaz.Scalaz._
 import scalaz.concurrent.Task
 
 object ScalazTaskCase extends SumResult {
+
+  val futures = Vector(Task(Math.sqrt(7891.12)), Task(Math.sqrt(995522.54)), Task(Math.sqrt(359.87)))
+
   override def sumResult: Double = {
-    val futures = Vector(Task(Math.sqrt(7891.12)), Task(Math.sqrt(995522.54)), Task(Math.sqrt(359.87)))
     val sum = for {
       v <- futures.sequence[Task, Double]
     } yield v.sum
